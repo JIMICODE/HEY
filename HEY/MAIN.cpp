48ad34e5aebbc14a63ed020afe272689174add73
JIMICODE <1159081156@qq.com>
2017-01-09 15:35:02 +0800

    kko

--- HEY/MAIN.cpp
+++ HEY/MAIN.cpp
@@ -23,11 +23,14 @@ bool Game_Init(HWND hwnd)
 
 	if (d3d == NULL)
 	{
-		MessageBox(hwnd, "Error initializing Direct3D", "Error", MB_OK);
+		MessageBox(hwnd, L"Error initializing Direct3D", L"Error", MB_OK);
 		return FALSE;
 	}
 
 	D3DPRESENT_PARAMETERS d3dpp;
+	//D3DDISPLAYMODE dm;
+
+	//d3d->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &dm);
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
 
 	d3dpp.Windowed = true;
@@ -42,43 +45,44 @@ bool Game_Init(HWND hwnd)
 		D3DADAPTER_DEFAULT,
 		D3DDEVTYPE_HAL,
 		hwnd,
-		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+		D3DCREATE_HARDWARE_VERTEXPROCESSING,
 		&d3dpp,
 		&d3ddev);
 
 	if (d3ddev == NULL)
 	{
-		MessageBox(hwnd, "Error creating Direct3D device", "Error", MB_OK);
+		MessageBox(hwnd, L"Error creating Direct3D device", L"Error", MB_OK);
 		return FALSE;
 	}
-	return true;
+	return TRUE;
 }
 
 void Game_Run(HWND hwnd)
 {
 	if (d3ddev == NULL)
 	{
-		MessageBox(hwnd, "NULL", "NULL", NULL);
+		MessageBox(hwnd, L"NULL", L"NULL", NULL);
 		return;
 	}
 
-	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), -1.0f, 0);
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 255, 255), 1.0f, 0);
 
 	if (d3ddev->BeginScene())
 	{
 		d3ddev->EndScene();
+		d3ddev->Present(NULL, NULL, hwnd, NULL);
 	}
-	d3ddev->Present(NULL, NULL, hwnd, NULL);
+	
 
 	if (KEY_DOWN(VK_ESCAPE))
 	{
-		MessageBox(hwnd, "ESC", "ESC", NULL);
+		MessageBox(hwnd, L"ESC", L"ESC", NULL);
 		PostMessage(hwnd, WM_DESTROY, 0, 0);
 	}
 
 	if (!isRun)
 	{
-		MessageBox(hwnd, "Running", "Fuck", NULL);
+		MessageBox(hwnd, L"Running", L"Fuck", NULL);
 		isRun = true;
 	}
 }
@@ -123,15 +127,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wc.lpszClassName = APPTITLE.c_str();
+	wc.lpszClassName = title;
 	wc.lpszMenuName = NULL;
 
 	if (!RegisterClassEx(&wc))
 		return FALSE;
 
 	HWND hwnd = CreateWindow(
-		APPTITLE.c_str(),
-		APPTITLE.c_str(),
+		title,
+		title,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
