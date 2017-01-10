b00cfa37fc40994282983a72a01ab4ed95169fb9
JIMICODE <1159081156@qq.com>
2017-01-10 15:05:27 +0800

    FullScreen to see the pic

--- HEY/MAIN.cpp
+++ HEY/MAIN.cpp
@@ -9,9 +9,9 @@ using namespace std;
 #pragma comment(lib, "d3dx9.lib")
 
 const string APPTITLE = "Direcet3D_Windowed";
-const wchar_t title[] = L"Create Surface Program";
+const wchar_t title[] = L"Draw Bitmap Program";
 const int SCREENW = 1024;
-const int SCREENH = 769;
+const int SCREENH = 768;
 
 LPDIRECT3D9 d3d = NULL;
 LPDIRECT3DDEVICE9 d3ddev = NULL;
@@ -30,16 +30,18 @@ bool Game_Init(HWND hwnd)
 		MessageBox(hwnd, L"Error initializing Direct3D", L"Error", MB_OK);
 		return false;
 	}
+	D3DDISPLAYMODE dm;
+	d3d->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &dm);
 
 	D3DPRESENT_PARAMETERS d3dpp;
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
 
-	d3dpp.Windowed = true;
+	d3dpp.Windowed = FALSE;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.BackBufferFormat = dm.Format;
 	d3dpp.BackBufferCount = 1;
-	d3dpp.BackBufferWidth = SCREENW;
-	d3dpp.BackBufferHeight = SCREENH;
+	d3dpp.BackBufferWidth = dm.Width;
+	d3dpp.BackBufferHeight = dm.Height;
 	d3dpp.hDeviceWindow = hwnd;
 
 	d3d->CreateDevice(
@@ -56,20 +58,31 @@ bool Game_Init(HWND hwnd)
 		return FALSE;
 	}
 
-	srand((unsigned int)time(NULL));
 	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 255, 255), 1.0f, 0);
 	d3ddev->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &backbuffer);
 
 	HRESULT result = d3ddev->CreateOffscreenPlainSurface(
-		100,
-		100,
+		1440,
+		900,
 		D3DFMT_X8R8G8B8,
 		D3DPOOL_DEFAULT,
 		&surface,
 		NULL
 	);
-
 	if (result != D3D_OK)	return false;
+
+	result = D3DXLoadSurfaceFromFile(
+		surface,
+		NULL,
+		NULL,
+		L"666/c.bmp",
+		NULL,
+		D3DX_DEFAULT,
+		0,
+		NULL
+	);
+	if (result != D3D_OK)	return false;
+
 	return true;
 }
 
@@ -83,30 +96,15 @@ void Game_Run(HWND hwnd)
 
 	if (d3ddev->BeginScene())
 	{
-		int r = rand() % 255;
-		int g = rand() % 255;
-		int b = rand() % 255;
-
-		d3ddev->ColorFill(surface, NULL, D3DCOLOR_XRGB(r, g, b));
-
-		RECT rect;
-		rect.left = rand() % SCREENW;
-		rect.right = rect.left + rand() % SCREENW / 2;
-		rect.top = rand() % SCREENH;
-		rect.bottom = rect.top + rand() % SCREENH / 2;
-
-		d3ddev->StretchRect(surface, NULL, backbuffer, &rect, D3DTEXF_NONE);
+		d3ddev->StretchRect(surface, NULL, backbuffer, NULL, D3DTEXF_NONE);
 		d3ddev->EndScene();
 		d3ddev->Present(NULL, NULL, NULL, NULL);
 	}
 	
-
 	if (KEY_DOWN(VK_ESCAPE))
 	{
-		MessageBox(hwnd, L"ESC", L"ESC", NULL);
 		PostMessage(hwnd, WM_DESTROY, 0, 0);
 	}
-
 }
 
 void Game_End(HWND hwnd)
@@ -160,11 +158,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow
 	HWND hwnd = CreateWindow(
 		title,
 		title,
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		SCREENW,
-		SCREENH,
+		WS_EX_TOPMOST | WS_POPUP,
+		0,
+		0,
+		1440,
+		900,
 		(HWND)NULL,
 		(HMENU)NULL,
 		hInstance,
